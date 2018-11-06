5f12bbef9fadd053200e3a87e67ee0251cbe3544
NadiaChiu <s0429y@hotmail.com>
2018-11-06 17:39:02 +0800

    no message

---
+++ main.py
@@ -0,0 +1,118 @@
+import requests
+from tqdm import tqdm
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+from write2db import Write2Sqlite
+
+URL_PTT_BASE = 'https://www.ptt.cc'
+URL_PTT_TRAVEL = URL_PTT_BASE + '/bbs/Gossiping/index.html'
+COOKIES = {'over18': '1'}
+
+
+def parser_post_info(url):
+    code = url.split('/')[-1]
+    board = url.split('/')[-2]
+    dict_post = {'board': board, 'code': code}
+    l_dict_push = []
+
+    res = requests.get(url, cookies=COOKIES)
+    if res.status_code != 200:
+        print(res.status_code)
+        print('Requests Get Failed 01')
+    soup = BeautifulSoup(res.text.encode('utf-8'), "html.parser")
+    main_content = soup.find(id='main-content')
+    if main_content is None:
+        print('')
+    metalines = main_content.select('div.article-metaline')
+    try:
+        author = metalines[0].select('span.article-meta-value')[0].text
+        title = metalines[1].select('span.article-meta-value')[0].text
+        dt = metalines[2].select('span.article-meta-value')[0].text
+        dict_post['author'] = author
+        dict_post['title'] = title
+        dict_post['dt'] = dt
+    except IndexError:
+        return dict_post, l_dict_push
+
+    # remove nodes : meta
+    for meta in metalines:
+        meta.extract()
+    for meta in main_content.select('div.article-metaline-right'):
+        meta.extract()
+
+    # remove node : f2
+    spans = main_content.find_all('span', class_='f2')
+    for span in spans:
+        span.extract()
+
+    # remove node : push
+    pushes = main_content.find_all('div', class_='push')
+    for push in pushes:
+        pushinfo = {'board': board}
+        try:
+            pushinfo['user'] = push.select('span.f3.hl.push-userid')[0].text
+            pushinfo['ipdt'] = push.select('span.push-ipdatetime')[0].text
+            pushinfo['tag'] = push.select('span.hl.push-tag')[0].text
+            pushinfo['content'] = push.select('span.f3.push-content')[0].text
+            l_dict_push.append(pushinfo)
+        except IndexError:
+            pass
+        push.extract()
+
+    content = ''
+    for item in main_content.contents:
+        if type(item) == Tag:
+            content += item.text
+        else:
+            content += str(item)
+    dict_post['content'] = content
+
+    return dict_post, l_dict_push
+
+
+def get_post_url(soup):
+    l_url = []
+    for line in soup.select('div.r-ent div.title a'):
+        try:
+            l_url.append(URL_PTT_BASE + line.attrs['href'])
+        except AttributeError:
+            pass  # 本文已被刪除
+    return l_url
+
+
+def get_next_page(soup):
+    try:
+        if 'href' in soup.select('.action-bar a.btn.wide')[1].attrs:
+            return URL_PTT_BASE + soup.select('.action-bar a.btn.wide')[1].attrs['href']
+        else:
+            return None
+    except Exception as e:
+        print(e)
+        return None
+
+
+if __name__ == '__main__':
+    _post_url = URL_PTT_TRAVEL
+    _l_post_url = []
+
+    # 先將每頁的po文url解出來
+    for _page in tqdm(range(1)):
+        _res = requests.get(_post_url, cookies=COOKIES)
+        if _res.status_code != 200:
+            print(_res.status_code)
+        _soup = BeautifulSoup(_res.text.encode('utf-8'), "html.parser")
+        _l_post_url.extend(get_post_url(_soup))
+
+        _post_url = get_next_page(_soup)
+
+    # create db writer
+    _db_writer = Write2Sqlite()
+
+    # 解po文 - single
+    for _url in tqdm(_l_post_url):
+        _dict_post, _l_dict_push = parser_post_info(_url)
+        # write to db
+        _db_writer.execute_data(_dict_post, _l_dict_push)
+
+    # close db
+    _db_writer.close()
---
+++ write2db.py
@@ -0,0 +1,89 @@
+# import pyodbc
+import os
+import sqlite3
+# import numpy as np
+
+# 發文表
+TABLE_POST_COL = ['id', 'board', 'code', 'author', 'title', 'dt', 'content']
+CREATE_TABLE_POST = 'CREATE TABLE IF NOT EXISTS pttpost(' \
+                    'id INTEGER, ' \
+                    'board TEXT,' \
+                    'code TEXT UNIQUE,' \
+                    'author TEXT,' \
+                    'title TEXT,' \
+                    'dt TEXT,' \
+                    'content TEXT, PRIMARY KEY (id))'
+DATA_INSERT_POST = 'INSERT INTO pttpost ({collist}) VALUES({values})'.format(
+    collist=','.join(TABLE_POST_COL), values=','.join(['?'] * len(TABLE_POST_COL)))
+
+# 推文表
+TABLE_PUSH_COL = ['pttpost_id', 'board', 'user', 'ipdt', 'tag', 'content']
+CREATE_TABLE_PUSH = 'CREATE TABLE IF NOT EXISTS pttpush(' \
+                    'pttpost_id INTEGER,' \
+                    'board TEXT,' \
+                    'user TEXT,' \
+                    'ipdt TEXT,' \
+                    'tag TEXT,' \
+                    'content TEXT)'
+DATA_INSERT_PUSH = 'INSERT INTO pttpush ({collist}) VALUES({values})'.format(
+    collist=','.join(TABLE_PUSH_COL), values=','.join(['?'] * len(TABLE_PUSH_COL)))
+
+
+class Write2Sqlite:
+    def __init__(self):
+        self._path = os.path.join(os.getcwd(), 'db\\mydb.db')
+        if os.path.exists(self._path):
+            os.remove(self._path)
+        else:
+            os.makedirs(os.path.join(os.getcwd(), 'db'))
+        self._conn = sqlite3.connect(self._path)
+        self._cursor = self._conn.cursor()
+        self._check_table()
+
+    def _check_table(self):
+        self._cursor.execute(CREATE_TABLE_POST)
+        self._cursor.execute(CREATE_TABLE_PUSH)
+        self._commit()
+
+    def _commit(self):
+        self._conn.commit()
+
+    def _rollback(self):
+        self._conn.rollback()
+
+    @staticmethod
+    def _check_data(dict_data, data_type):
+        if data_type == 'POST':
+            return tuple([dict_data[i] for i in TABLE_POST_COL])
+        elif data_type == 'PUSH':
+            return [tuple([data[item] for item in TABLE_PUSH_COL]) for data in dict_data]
+        else:
+            return None
+
+    def execute_data(self, dict_post, l_dict_push):
+        try:
+            # get post id
+            post_id = self._cursor.execute('SELECT MAX(id) FROM pttpost').fetchall()
+            post_id = post_id[0][0] + 1 if len(post_id) == 1 and post_id[0][0] is not None else 1
+
+            # insert id info
+            dict_post['id'] = post_id
+            for item in l_dict_push:
+                item['pttpost_id'] = post_id
+
+            # insert data : post
+            ins = self._check_data(dict_post, 'POST')
+            self._cursor.execute(DATA_INSERT_POST, ins)
+
+            # insert data : push
+            ins = self._check_data(l_dict_push, 'PUSH')
+            self._cursor.executemany(DATA_INSERT_PUSH, ins)
+
+            self._commit()
+        except Exception as e:
+            print(e)
+            self._rollback()
+
+    def close(self):
+        self._cursor.close()
+        self._conn.close()
